7d028792cd0b328b57633e76c957dc418f1762b7
Chris Brown <chris.brown@thalmic.com>
2020-05-10 11:38:53 -0400

    Fix fw env

--- fw/env.bat
+++ fw/env.bat
@@ -1,2 +1,3 @@
 cd /d %~dp0
-cmd.exe /K "set PATH=bin;%PATH% && %XILINX_SDK_ROOT%/settings64.bat"
+rem cmd.exe /K "set PATH=bin;%PATH% && %XILINX_SDK_ROOT%/settings64.bat"
+cmd.exe /K "set PATH=%MSYS2_BIN%;%SystemRoot%\system32 && %XILINX_SDK_ROOT%/settings64.bat"
